af0f56614d0fab3a06b324255c1072f044fcc821
240394381@qq.com <240394381@qq.com>
2018-12-15 15:11:06 +0800

    Initial Commit

---
+++ ZXCalculation/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ZXCalculation
+//
+//  Created by xinying on 2018/12/15.
+//  Copyright © 2018年 habav. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
